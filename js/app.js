239ee80cbcbdcd5db36c4f7681de034d91b220ac
Erica Bruenton <ercfit@gmail.com>
2018-06-25 17:16:39 -0600

    constructor, name, image, metrics

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
 $('document').ready(function(){
 console.log("allez...");
 
-class Tomagotchi {
+class Tomagatchi {
+	constructor(name, age, hunger, sleep, bordom){
+		this.name = name;
+		this.age = age;
+		this.hunger = hunger;
+		this.sleep = sleep;
+		this.bordom = bordom;
+	}
+	hungry(){
+		console.log("I'm hungry!");
+	}
 
+	tired(){
+		console.log("I'm sleepy!");
+	}
+
+	bored(){
+		console.log("I'm bored...");
+	}
 
 };
 
-const nubia = new Tomagotchi();
-	console.log(nubia);
-
+const tg1 = new Tomagatchi();
+	console.log(tg1);
+	console.log(typeof(tg1));
+	hunger = 5,
+	sleep = 9,
+	bordom = 1,
+	age = 7;
 
 });
