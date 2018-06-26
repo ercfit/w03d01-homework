ae261aa3cee3945ecb22cb902a1e48d98f242b92
Erica Bruenton <ercfit@gmail.com>
2018-06-26 13:56:06 -0600

    age interval, death function

--- js/app.js
+++ js/app.js
@@ -8,34 +8,46 @@ console.log("allez...");
 			this.hunger = 0;
 			this.sleep = 0;
 			this.bordom = 0;
-		
+		}
 			hungry(){
 				if(this.hunger > 1){
 					this.hunger--;
 				}
-				console.log("I'm hungry!");
+				console.log("I'm hungry!", this.hunger);
 			}
 
 			tired(){
 				if(this.sleep > 1){
 					this.sleep--;
 				}
-				console.log("I'm sleepy!");
+				console.log("I'm sleepy!", this.sleep);
 			}
 
 			bored(){
 				if(this.bordom > 1){
 					this.bordom--;
 				}
-				console.log("I'm bored...");
+				console.log("I'm bored...", this.bordom);
 			}
 
+			setName(){
+				//insert option to accept input
+				console.log(`Your Tomagatchi's name is ${this.name} `);
+			}
+
+			incrAge(){
+				this.age++;
+			}
+
+			
+			
+
 			upperRoom(){
-				if(this.hunger < 1 || this.tired < 1 || this.bordom < 1){
+				if(this.hunger > 9 || this.sleep > 9 || this.bordom > 9){
 					console.log(`${this.name} is dead. You should re-evaluate your decision to be a pet owner.`);
 				}
 			}
-		}
+		
 
 	}
 
@@ -43,11 +55,38 @@ console.log("allez...");
 			const testTg = new Tomagatchi();
 			console.log(testTg);
 			console.log(typeof(testTg));
-			testTg.hunger(2);
-			testTg.sleep(9);
-			testTg.bordom(1);
-			testTg.age(7);
-			testTg.name("Benedict Cumberbatch");
+			testTg.hungry(2);
+			testTg.tired(11);
+			testTg.bored(1);
+			testTg.upperRoom();
+
+			//prints starting age which was set in constructor
+				console.log(testTg.age);
+
+			//set name function
+			//prompt user for name (in HTML--see 6/26 lesson)--then testTG.name = user input....
+			testTg.name = ("Benedict Cumberbatch");
+			testTg.setName(testTg.name);
+
+			//tesing increaseAge
+			testTg.incrAge(testTg.age);
+				console.log(testTg.age);
+
+			//age increases w/ time	(every 5 sec)
+			var ageIntv  = setInterval(testTg.incrAge(), 5000);
+				console.log(ageIntv);
+
+			// //hunger increases w/ time	(every 5 sec)
+			// var ageIntv  = setInterval(testTg.incrAge(), 5000);
+			// 	console.log(ageIntv);
+				
+			// //sleep increases w/ time	(every 5 sec)
+			// var ageIntv  = setInterval(testTg.incrAge(), 5000);
+			// 	console.log(ageIntv);	
+
+			// //boredom increases w/ time	(every 5 sec)
+			// var ageIntv  = setInterval(testTg.incrAge(), 5000);
+			// 	console.log(ageIntv);
 
 
 			const $btn1 = $('#btn1');
@@ -72,7 +111,10 @@ console.log("allez...");
 				seconds++;
 			}
 
-			timePasses = setIntv(timePassing, 3000);
-			clearIntv(timePasses);
+
+
+			
+			// timePasses = setInterval(timePassing, 3000);
+			// clearIntv(timePasses);
 	
 
